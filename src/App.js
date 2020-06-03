e66c267842fce0d89f53dc93387789586a659310
Lucas Assis Vieira <lucasav2005@hotmail.com>
2020-06-03 20:40:41 -0300

    Finalizado projeto de conceitos de react

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+import "./styles.css";
+
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', { title: `Repo ${Date.now()}`});
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    const repositoriesWithoutDeleted = repositories.filter(repo => repo.id !== id);
+
+    setRepositories([...repositoriesWithoutDeleted]);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
